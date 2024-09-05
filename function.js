1ffbc07c56aa9679927b6006199bf52b551f61c5
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-05 22:06:01 +0600

    function example

--- function.js
+++ function.js
@@ -1,6 +1,25 @@
-function firstFunction() {
-    console.log("I can make function");
-    console.log("This is the proof");
+// function firstFunction() {
+//     console.log("I can make function");
+//     console.log("This is the proof");
     
+// }
+// firstFunction();
+// function secondFunction() {
+//     console.log("I can make function");
+//     console.log("This is the proof");
+    
+// }
+// secondFunction();
+
+
+function sum(x,y) { 
+    let result= x-y;
+    return result;
 }
-firstFunction();
+
+let total=sum(10,5);
+console.log(total);
+
+let divide=sum(20,2);
+console.log(divide);
+
