7b6994204ffe6d394f69fd24770942a974e3fc1a
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-07 00:05:04 +0600

    function return

--- function.js
+++ function.js
@@ -32,18 +32,41 @@
 // square(2);
 
 
-function doubleIt(number) {
-    const doubled=number*2;
-    console.log(number,doubled);
-}
-console.log('I will call the function')
-doubleIt(15)
-doubleIt(20)
-doubleIt(30)
-console.log("...............");
+// function doubleIt(number) {
+//     const doubled=number*2;
+//     console.log(number,doubled);
+// }
+// console.log('I will call the function')
+// doubleIt(15)
+// doubleIt(20)
+// doubleIt(30)
+// console.log("...............");
 
-const num=10;
-doubleIt(num)
+// const num=10;
+// doubleIt(num)
 
 
  
+
+
+// function test(num,num2){
+//   const number= num-num2;
+//   console.log('subtraction between num to num2:', number);
+  
+// }
+
+// const first=10;
+// const second =2;
+// test(first,second);
+
+
+
+// return
+function fiveTimes(num) {
+    const sum =num*5;
+    return sum;
+}
+
+fiveTimes(5);
+const total=fiveTimes(5);
+console.log(total);
